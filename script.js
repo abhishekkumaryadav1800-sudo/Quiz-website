3db4f074bec68f9ff36d6b2e1c757096083823fa
abhishekkumaryadav1800-sudo <abhishekkumaryadav1800@gmail.com>
2026-07-16 18:21:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
+function startQuiz(){
 
-async function loadQuiz() {
-    const response = await fetch(
-        "https://opentdb.com/api.php?amount=1&type=multiple"
-    );
+const name =
+document.getElementById("playerName").value;
 
-    const data = await response.json();
-    const q = data.results[0];
-
-    document.getElementById("question").innerHTML = q.question;
-
-    let answers = [...q.incorrect_answers];
-    answers.push(q.correct_answer);
-
-    answers.sort(() => Math.random() - 0.5);
-
-    let html = "";
-
-    answers.forEach(answer => {
-        html += `
-        <button class="option">
-            ${answer}
-        </button><br><br>
-        `;
-    });
-
-    document.getElementById("options").innerHTML = html;
+if(name===""){
+alert("Enter Name");
+return;
 }
 
-if(window.location.pathname.includes("quiz.html")){
-    loadQuiz();
+document.getElementById("quizBox").style.display="block";
+
+showQuestion();
 }
