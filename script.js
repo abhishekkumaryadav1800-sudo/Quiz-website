25dd0be6b2f5213158df700e535ac64bba881235
abhishekkumaryadav1800-sudo <abhishekkumaryadav1800@gmail.com>
2026-07-16 18:33:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,79 @@
-function startQuiz(){
+let current = 0;
+let score = 0;
 
-const name =
-document.getElementById("playerName").value;
+function startQuiz() {
 
-if(name===""){
-alert("Enter Name");
+const name = document.getElementById("playerName").value;
+
+if (name.trim() === "") {
+alert("Enter Your Name");
 return;
 }
 
-document.getElementById("quizBox").style.display="block";
+document.getElementById("quizBox").style.display = "block";
 
 showQuestion();
+}
+
+function showQuestion() {
+
+let q = questions[current];
+
+document.getElementById("question").innerText = q.q;
+
+let html = "";
+
+q.o.forEach(opt => {
+
+html += `
+<button class="option" onclick="checkAnswer('${opt}')">
+${opt}
+</button>
+`;
+
+});
+
+document.getElementById("options").innerHTML = html;
+}
+
+function checkAnswer(ans) {
+
+const buttons = document.querySelectorAll(".option");
+
+buttons.forEach(btn => {
+
+btn.disabled = true;
+
+if (btn.innerText === questions[current].a) {
+btn.style.backgroundColor = "green";
+}
+
+if (btn.innerText === ans && ans !== questions[current].a) {
+btn.style.backgroundColor = "red";
+}
+
+});
+
+if (ans === questions[current].a) {
+score++;
+}
+}
+
+function nextQuestion() {
+
+current++;
+
+if (current < questions.length) {
+showQuestion();
+} else {
+finishQuiz();
+}
+}
+
+function finishQuiz() {
+
+document.getElementById("quizBox").style.display = "none";
+
+document.getElementById("result").innerHTML =
+`Your Score: ${score}/${questions.length}`;
 }
